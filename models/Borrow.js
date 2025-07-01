eba96f330c58f7a284d6485dcae736b099e9cac8
AkbarOktaviadi89 <Akbaroktaviadi89@gmail.com>
2025-07-01 20:33:02 +0700

    feat: add fine column for borrow models

--- models/Borrow.js
+++ models/Borrow.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const borrowSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   book: { type: mongoose.Schema.Types.ObjectId, ref: 'Book' },
-  borrowDate: { type: Date, default: Date.now }
+  borrowDate: { type: Date, default: Date.now },
+  returnDate: Date,
+  returnRequested: { type: Boolean, default: false },
+  isReturned: { type: Boolean, default: false },
+  fine: { type: Number, default: 0 }
 });
+
 
 module.exports = mongoose.model('Borrow', borrowSchema);
